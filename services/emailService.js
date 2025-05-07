127bbadf2683d9f4ef6a97588bdfde11e3899f1b
adimilsonf <149102076+adimilsonf@users.noreply.github.com>
2025-05-07 18:26:39 -0300

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -80,7 +80,7 @@ async function sendPaymentConfirmation(email, trackingCode, nomeCompleto, phone,
     `;
 
     const info = await transporter.sendMail({
-        from: `"Giuliana Flores - Transportadora Aurora" <${process.env.SMTP_USER}>`,
+        from: `"Mercado Livre - Transportadora Aurora" <${process.env.SMTP_USER}>`,
         to: email,
         subject: "Seu pagamento foi aprovado!",
         html: htmlContent,
